dc2c06345d68139add674a9f78ed5da6fcadb005
Adam Jermyn <adamjermyn@gmail.com>
2022-01-26 11:07:34 -0500

    Fixed version bug

--- run_grid.sh
+++ run_grid.sh
@@ -2,7 +2,7 @@
 
 export MESASDK_ROOT="/mnt/home/ajermyn/Software/MESA/mesasdk"
 source $MESASDK_ROOT/bin/mesasdk_init.sh
-export MESA_DIR="/mnt/home/ajermyn/Software/MESA/mesa-15140"
+export MESA_DIR="/mnt/home/ajermyn/Software/MESA/mesa-r21.12.1"
 export OMP_NUM_THREADS=2
 
 
